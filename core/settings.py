b966262f2b04595484df2fee14cad4599e64e248
priyaljain03 <jainpriyal03@gmail.com>
2022-03-10 02:13:51 +0530

    Changes ACL setting to None

--- core/settings.py
+++ core/settings.py
@@ -128,7 +128,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_STORAGE_BUCKET_NAME = 'django-netflix-clone'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl':'max-age=86400'}
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
 # AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
